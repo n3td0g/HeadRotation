ed1d5fd34e5285f499ce572e7f9b7e604cd578f0
n3td0g <n3td0g@mail.ru>
2018-02-25 16:29:09 +0300

    Added render control

---
+++ HeadRotation/Controls/RenderControl.Designer.cs
@@ -0,0 +1,61 @@
+﻿using OpenTK;
+namespace HeadRotation.Controls
+{
+    partial class RenderControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.glControl = new OpenTK.GLControl();
+            this.SuspendLayout();          
+            // 
+            // glControl
+            // 
+            this.glControl.AllowDrop = true;
+            this.glControl.BackColor = System.Drawing.Color.Black;
+            this.glControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.glControl.Location = new System.Drawing.Point(0, 0);
+            this.glControl.Name = "glControl";
+            this.glControl.Size = new System.Drawing.Size(728, 493);
+            this.glControl.TabIndex = 3;
+            this.glControl.VSync = false;
+            // 
+            // RenderControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.glControl);
+            this.Name = "RenderControl";
+            this.Size = new System.Drawing.Size(539, 452);           
+            this.ResumeLayout(false);            
+
+        }
+
+        private GLControl glControl;
+
+        #endregion
+    }
+}
