363ac35d1b622984784af2e1480208493f9fcda5
n3td0g <n3td0g@mail.ru>
2018-03-16 23:23:26 +0300

    Отражение точек

--- HeadRotation/Helpers/MorphHelper.cs
+++ HeadRotation/Helpers/MorphHelper.cs
@@ -16,6 +16,39 @@ namespace HeadRotation.Helpers
         private Vector3 topVector = new Vector3(0.0f, 1.0f, 0.0f);
 
         static List<int> headIndices = new List<int>();
+        static List<int> mirroredPoints = new List<int>
+        {
+            12, 15,
+            18, 21,
+            16, 17,
+            19, 20,
+            13, 14,
+            23, 26,
+            35, 40,
+            28, 32,
+            36, 39,
+            24, 25,
+            38, 41,
+            27, 31,
+            37, 42,
+            0, 1,
+            66, 67,
+            68, 69,
+            5, 6,
+            7, 8,
+            9, 10,
+            43, 44,
+            45, 46,
+            47, 48,
+            50, 51,
+            52, 53,
+            56, 57,
+            3, 4,
+            60, 62,
+            63, 65,
+            58, 59
+        };
+
         //new List<int> { 66, 67, 68, 69, 5, 6, 7, 8, 9, 10, 11 };
         private Matrix4 RotationMatrix;
 
@@ -36,7 +69,9 @@ namespace HeadRotation.Helpers
             rightVector = headPoints.GetWorldPoint(new Vector3(1.0f, 0.0f, 0.0f));
             forwardVector = headPoints.GetWorldPoint(new Vector3(0.0f, 0.0f, 1.0f));
 
-            ProcessHeadPoints();
+            //ProcessHeadPoints();
+
+            //MirrorPoints(headPoints.HeadMesh.HeadAngle > 0.0f);
         }
 
         private void ProcessHeadPoints()
@@ -53,6 +88,20 @@ namespace HeadRotation.Helpers
                 result.X = c.X;
                 result.Z = c.Z;
                 headPoints.Points[index] = Vector4.Transform(new Vector4(result), RotationMatrix).Xyz;
+            }
+        }
+
+        private void MirrorPoints(bool leftToRight)
+        {
+            for(int i = 0; i < mirroredPoints.Count; i += 2)
+            {
+                int a = mirroredPoints[leftToRight ? i : i + 1];
+                int b = mirroredPoints[leftToRight ? i + 1 : i];
+                var vectorA = headPoints.Points[a];
+                var vectorB = headPoints.Points[b];
+                vectorB = vectorA;
+                vectorB.X = -vectorB.X;
+                headPoints.Points[b] = vectorB;
             }
         }
     }
