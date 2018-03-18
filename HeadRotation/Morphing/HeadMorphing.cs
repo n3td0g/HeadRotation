72d21a8684136942720d8c75849dbae774fa1931
genki <reward.hunters@gmail.com>
2018-03-18 17:06:05 +0500

    Треугольники для профиля.

--- HeadRotation/Morphing/HeadMorphing.cs
+++ HeadRotation/Morphing/HeadMorphing.cs
@@ -3,6 +3,7 @@ using HeadRotation.Render;
 using OpenTK;
 using OpenTK.Graphics.OpenGL;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace HeadRotation.Morphing
 {
@@ -17,7 +18,7 @@ namespace HeadRotation.Morphing
     {
         public List<MorphTriangle> TrianglesFront = new List<MorphTriangle>();
         public List<MorphTriangle> TrianglesRight = new List<MorphTriangle>();
-        public HeadPoints headPoints;       
+        public HeadPoints headPoints;
 
         public void Initialize(HeadPoints hPoints)
         {
@@ -44,9 +45,11 @@ namespace HeadRotation.Morphing
             headPoints.Points.Add((b2 + b3) * 0.5f); //75
             headPoints.Points.Add(b3); //76
             headPoints.Points.Add((b3 + b) * 0.5f); //77
-            
+
             headPoints.Points.Add(a1); //78
             headPoints.Points.Add(a2); //79
+
+            headPoints.IsVisible.AddRange(Enumerable.Repeat(true, 10));
 
             #region TrianglesFront
             // left oval
@@ -70,7 +73,7 @@ namespace HeadRotation.Morphing
             TrianglesFront.Add(new MorphTriangle { A = 72, B = 71, C = 16 });
             TrianglesFront.Add(new MorphTriangle { A = 66, B = 12, C = 23 });
             TrianglesFront.Add(new MorphTriangle { A = 66, B = 52, C = 45 });
-            TrianglesFront.Add(new MorphTriangle { A = 73, B = 74, C = 68});
+            TrianglesFront.Add(new MorphTriangle { A = 73, B = 74, C = 68 });
             TrianglesFront.Add(new MorphTriangle { A = 74, B = 75, C = 9 });
 
             //right oval
@@ -206,6 +209,74 @@ namespace HeadRotation.Morphing
 
             #endregion
 
+            #region Triangles right
+
+            TrianglesRight.Add(new MorphTriangle { A = 5, B = 7, C = 78 });
+            TrianglesRight.Add(new MorphTriangle { A = 9, B = 7, C = 78 });
+            TrianglesRight.Add(new MorphTriangle { A = 9, B = 11, C = 78 });
+            TrianglesRight.Add(new MorphTriangle { A = 11, B = 75, C = 78 });
+            TrianglesRight.Add(new MorphTriangle { A = 5, B = 3, C = 68 });
+            TrianglesRight.Add(new MorphTriangle { A = 66, B = 12, C = 79 });
+            TrianglesRight.Add(new MorphTriangle { A = 66, B = 43, C = 45 });
+            TrianglesRight.Add(new MorphTriangle { A = 66, B = 45, C = 68 });
+            TrianglesRight.Add(new MorphTriangle { A = 50, B = 45, C = 68 });
+            TrianglesRight.Add(new MorphTriangle { A = 50, B = 52, C = 68 });
+            TrianglesRight.Add(new MorphTriangle { A = 68, B = 52, C = 3 });
+            TrianglesRight.Add(new MorphTriangle { A = 5, B = 7, C = 3 });
+            TrianglesRight.Add(new MorphTriangle { A = 7, B = 63, C = 3 });
+            TrianglesRight.Add(new MorphTriangle { A = 55, B = 75, C = 11 });
+            TrianglesRight.Add(new MorphTriangle { A = 55, B = 58, C = 11 });
+            TrianglesRight.Add(new MorphTriangle { A = 9, B = 58, C = 11 });
+            TrianglesRight.Add(new MorphTriangle { A = 9, B = 63, C = 7 });
+            TrianglesRight.Add(new MorphTriangle { A = 63, B = 58, C = 9 });
+
+            TrianglesRight.Add(new MorphTriangle { A = 12, B = 79, C = 18 });
+            TrianglesRight.Add(new MorphTriangle { A = 72, B = 79, C = 18 });
+            TrianglesRight.Add(new MorphTriangle { A = 72, B = 16, C = 18 });
+            TrianglesRight.Add(new MorphTriangle { A = 72, B = 16, C = 73 });
+            TrianglesRight.Add(new MorphTriangle { A = 73, B = 13, C = 16 });
+            TrianglesRight.Add(new MorphTriangle { A = 73, B = 13, C = 22 });
+            TrianglesRight.Add(new MorphTriangle { A = 73, B = 2, C = 22 });
+
+            TrianglesRight.Add(new MorphTriangle { A = 2, B = 49, C = 47 });
+            TrianglesRight.Add(new MorphTriangle { A = 2, B = 43, C = 47 });
+            TrianglesRight.Add(new MorphTriangle { A = 45, B = 43, C = 47 });
+            TrianglesRight.Add(new MorphTriangle { A = 45, B = 49, C = 47 });
+            TrianglesRight.Add(new MorphTriangle { A = 50, B = 49, C = 45 });
+            TrianglesRight.Add(new MorphTriangle { A = 50, B = 52, C = 56 });
+            TrianglesRight.Add(new MorphTriangle { A = 3, B = 52, C = 56 });
+
+            TrianglesRight.Add(new MorphTriangle { A = 2, B = 43, C = 22 });
+            TrianglesRight.Add(new MorphTriangle { A = 37, B = 43, C = 22 });
+            TrianglesRight.Add(new MorphTriangle { A = 37, B = 43, C = 66 });
+            TrianglesRight.Add(new MorphTriangle { A = 37, B = 23, C = 66 });
+            TrianglesRight.Add(new MorphTriangle { A = 12, B = 23, C = 66 });
+            TrianglesRight.Add(new MorphTriangle { A = 35, B = 23, C = 12 });
+            TrianglesRight.Add(new MorphTriangle { A = 35, B = 23, C = 37 });
+            TrianglesRight.Add(new MorphTriangle { A = 35, B = 22, C = 37 });
+            TrianglesRight.Add(new MorphTriangle { A = 35, B = 13, C = 12 });
+            TrianglesRight.Add(new MorphTriangle { A = 35, B = 13, C = 22 });
+            TrianglesRight.Add(new MorphTriangle { A = 18, B = 13, C = 12 });
+            TrianglesRight.Add(new MorphTriangle { A = 18, B = 13, C = 16 });
+
+            TrianglesRight.Add(new MorphTriangle { A = 49, B = 50, C = 56 });
+            TrianglesRight.Add(new MorphTriangle { A = 49, B = 61, C = 56 });
+            TrianglesRight.Add(new MorphTriangle { A = 3, B = 61, C = 56 });
+            TrianglesRight.Add(new MorphTriangle { A = 3, B = 60, C = 63 });
+
+            TrianglesRight.Add(new MorphTriangle { A = 58, B = 64, C = 63 });
+            TrianglesRight.Add(new MorphTriangle { A = 58, B = 64, C = 55 });
+
+            TrianglesRight.Add(new MorphTriangle { A = 49, B = 2, C = 75 });            // сомнительные губищи
+            TrianglesRight.Add(new MorphTriangle { A = 49, B = 61, C = 75 });
+            TrianglesRight.Add(new MorphTriangle { A = 64, B = 61, C = 75 });
+            TrianglesRight.Add(new MorphTriangle { A = 64, B = 55, C = 75 });
+
+            TrianglesRight.Add(new MorphTriangle { A = 60, B = 61, C = 63 });
+            TrianglesRight.Add(new MorphTriangle { A = 61, B = 64, C = 63 });
+
+            #endregion
+
             InitializeMorphin();
         }
 
@@ -236,7 +307,7 @@ namespace HeadRotation.Morphing
             {
                 foreach (var point in part.MorphPoints)
                 {
-                    if(point.FrontTriangle.TriangleIndex > -1 )
+                    if (point.FrontTriangle.TriangleIndex > -1)
                     {
                         var triangle = TrianglesFront[point.FrontTriangle.TriangleIndex];
                         var a = headPoints.Points[triangle.A].Xy;
@@ -245,7 +316,7 @@ namespace HeadRotation.Morphing
 
                         point.Morph(ref a, ref b, ref c);
 
-                        foreach(var index in point.Indices)
+                        foreach (var index in point.Indices)
                         {
                             part.Vertices[index].Position = point.Position;
                         }
@@ -260,8 +331,10 @@ namespace HeadRotation.Morphing
             GL.Begin(PrimitiveType.Triangles);
             GL.Color4(0.0f, 1.0f, 0.0f, 0.3f);
 
+        
+
             var points = useProfilePoints ? TrianglesRight : TrianglesFront;
-            foreach (var triangle in TrianglesFront)
+            foreach (var triangle in points)
             {
                 var a = headPoints.GetWorldPoint(triangle.A);
                 var b = headPoints.GetWorldPoint(triangle.B);
@@ -279,7 +352,7 @@ namespace HeadRotation.Morphing
             GL.Begin(PrimitiveType.Lines);
 
             var points = useProfilePoints ? TrianglesRight : TrianglesFront;
-            foreach (var triangle in TrianglesFront)
+            foreach (var triangle in points)
             {
                 var a = headPoints.GetWorldPoint(triangle.A);
                 var b = headPoints.GetWorldPoint(triangle.B);
